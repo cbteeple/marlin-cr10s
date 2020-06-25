1a52761334f115939e2955420ff53aeccac41725
cbteeple <cbteeple@g.harvard.edu>
2020-06-25 12:59:59 -0400

    updated jog speeds

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -756,7 +756,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 2500, 2500, 100, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 2500, 2500, 200, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
